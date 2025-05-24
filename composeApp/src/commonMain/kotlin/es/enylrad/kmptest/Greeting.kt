5f96eff61e5e5b0cf5645e11883efad2e53eecb5
David <enylrad@gmail.com>
2025-05-24 19:33:36 +0200

    Refactor: Remove sample code and initial setup
    This commit removes the initial sample code, including:
    - Greeting class
    - Platform interface and implementations
    - Button and AnimatedVisibility in App composable

--- composeApp/src/commonMain/kotlin/es/enylrad/kmptest/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package es.enylrad.kmptest
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/es/enylrad/kmptest/Platform.kt
+++
@@ -1,7 +0,0 @@
-package es.enylrad.kmptest
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
